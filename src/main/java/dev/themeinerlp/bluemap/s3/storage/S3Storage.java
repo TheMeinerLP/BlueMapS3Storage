1cf4c2161e87dd5be999be73a0ed953c49ec1e9a
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2025-08-14 10:28:16 +0200

    feat(s3): Update S3StorageConfiguration with default values for bucket, region, and authentication

--- src/main/java/dev/themeinerlp/bluemap/s3/storage/S3Storage.java
+++ src/main/java/dev/themeinerlp/bluemap/s3/storage/S3Storage.java
@@ -1,3 +1,20 @@
+/**
+ * A simple storage implementation for bluemap to save data into s3 storage solution.
+ * Copyright (C) 2025 TheMeinerLP and contributors
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package dev.themeinerlp.bluemap.s3.storage;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
@@ -6,7 +23,6 @@ import de.bluecolored.bluemap.core.storage.MapStorage;
 import de.bluecolored.bluemap.core.storage.Storage;
 import de.bluecolored.bluemap.core.storage.compression.Compression;
 import de.bluecolored.bluemap.core.storage.file.FileMapStorage;
-
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
